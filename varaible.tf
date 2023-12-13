f1da4ad412016e65ccb6772b9ce2a54f9ab4080f
Kwasi200 <kwasisakyi@yahoo.com>
2023-12-13 00:36:58 +0000

    Initial commit

---
+++ varaible.tf
@@ -0,0 +1,10 @@
+variable "cluster-name" {
+ #default = "levelup-tf-eks-demo"
+  default = "rodeo-eks"
+  type    = string
+}
+
+variable "AWS_REGION" {
+# default = "eu-west-1"
+  default = "eu-west-3"
+}
